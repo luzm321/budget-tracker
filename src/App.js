cfeaec75b54b79d1a5de58ab7d437a6b11c75c40
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-10-11 17:56:03 -0500

    initial commit; added basic component html/css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import { Button, Stack } from "react-bootstrap";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        {/* me-auto (margin end) ensures the Budget header is always on the left side of the screen */}
+        {/* <h1>Budget Tracker</h1> */}
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="success">Add Budget</Button>
+        <Button variant="outline-success">Add Expense</Button>
+      </Stack>
+    </Container>
   );
-}
+};
 
 export default App;
