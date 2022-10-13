98e13a0c5ace0722c345449670740a9e1969e0ca
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-10-13 17:17:02 -0500

    added methods to delete a budget and expense

--- src/contexts/BudgetsProvider.js
+++ src/contexts/BudgetsProvider.js
@@ -16,8 +16,8 @@ export const BudgetsProvider = ({ children }) => {
     return expenses.filter((expense) => expense.budgetId === budgetId);
   };
 
-  const addExpense = ({ description, amount, budgetId}) => {
-    // set the expensess by taking current expenses (prevExpenses array), then returning them with addition of creating a new expense
+  const addExpense = ({ description, amount, budgetId }) => {
+    // set the expenses by taking current expenses (prevExpenses array), then returning them with addition of creating a new expense
     // with a new id that comes from the universal unique identifier library, along with its desc, amount, and budgetId value passed to fxn:
     setExpenses((prevExpenses) => {
       return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
@@ -29,16 +29,27 @@ export const BudgetsProvider = ({ children }) => {
     // with a new id that comes from the universal unique identifier library, along with its name and max value passed to fxn:
     setBudgets((prevBudgets) => {
       // check to prevent duplicate budgets
-      if (prevBudgets.find(budget => budget.name === name)) {
+      if (prevBudgets.find((budget) => budget.name === name)) {
         return prevBudgets;
-      };
+      }
       return [...prevBudgets, { id: uuidV4(), name, max }];
     });
   };
 
-  const deleteBudget = () => {};
+  const deleteBudget = ({ budgetId }) => {
+    // TODO: Deal with expenses (uncategorized budget)
+    // deleting budget that does not have the current id
+    setBudgets((prevBudgets) => {
+      return prevBudgets.filter((budget) => budget.id !== budgetId);
+    });
+  };
 
-  const deleteExpense = () => {};
+  const deleteExpense = ({ expenseId }) => {
+    // deleting expense that does not have the current id
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== expenseId);
+    });
+  };
 
   return (
     <BudgetsContext.Provider
